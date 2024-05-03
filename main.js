6dbf3ad21fa1d0adef3f2d9fe8c123ab6816f6f6
Ahmed Obeid <44338581+obeid546@users.noreply.github.com>
2024-05-03 23:06:02 +0300

    Fundamental Syntax (Course of Udemy)

---
+++ main.js
@@ -0,0 +1,112 @@
+var myNumber =50;
+
+if (myNumber <= 20) {
+    console.log ("you got the first case")
+} else if (myNumber >= 100) {
+    console.log("you got the second case");
+} else {
+    console.log("You got the final case!")
+}
+
+var myString = "Hello World";
+console.log (myString + ", How are you?");
+console.log (myString.length);
+console.log (myString[myString.length-1]);
+
+var myNumber = 20;
+var myDecimal = 400.3665;
+
+var result = (5 + 7) / 2;
+
+console.log (result);
+
+
+var myBoolean = true;
+
+var invertedBoolean = !myBoolean;
+
+var result = myBoolean === invertedBoolean
+
+if (result) {"myBoolean === invertedBoolean"};
+
+console.log (result);
+
+
+var myNull = null;
+
+console.log (myNull);
+
+var a = 10;
+var b = 20;
+var c = 10;
+
+if (a === b) {
+    console.log("First Condition");
+} else {
+    console.log("condition not met");
+}
+
+if (a === b || a === c) {
+    console.log("First Condition");
+} else {
+    console.log("condition not met");
+}
+
+
+if (a === b && a === c) {           //if (a === b && c) 
+    console.log("First Condition");
+} else {
+    console.log("condition not met");
+}
+
+
+if ((a === b && a === c) || a === c) {        
+    console.log("First Condition");
+} else {
+    console.log("condition not met");
+}
+
+if (!(a === b)) {        
+    console.log("First Condition");
+} else {
+    console.log("condition not met");
+}
+
+
+string = "Muslim and amitious. Software Engineer, Data Analysis Specialist and Fan, Science Communicator, and human sometimes. "
+
+console.log(string.length);
+
+/*var myNumber = 20;
+
+if (myNumber > 20) {
+    console.log ("Your number is greater than 20!")
+} else if (myNumber < 20) {
+    console.log("Your number is less than 20!");
+} else {
+    console.log("Your number is exactly 20!")
+}
+*/
+
+
+
+
+
+
+
+
+/*var myName = "Yousuf";
+
+    if (myName === "Ahmad")
+
+    {
+        console.log ("Hello Ahmad!");
+    } else if (myName === "Moha") {
+
+        console.log("you're a liar");
+    } else {
+
+        console.log ("You're a big liar");
+    }
+
+console.log(myName);*/
